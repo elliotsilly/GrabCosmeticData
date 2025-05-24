a7aa051956f6bdf324679bccab7b79eec62e7518
elliot! <benjikrolfanpageuwu@gmail.com>
2025-05-24 14:50:15 +0100

    Add files via upload

---
+++ GrabCosmeticData/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GrabCosmeticData
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.elliot.gorillatag.grabcosmeticdata";
+		public const string Name = "GrabCosmeticData";
+		public const string Version = "1.0.0";
+	}
+}
